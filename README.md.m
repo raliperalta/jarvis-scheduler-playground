9857f68151f23084f4f2c61c82eee7d064b9c4d9
raliperalta <rachid.aliperalta@henryscheinone.com>
2026-01-14 09:55:41 -0600

    Create README.md.m
    adding readme

---
+++ README.md.m
@@ -0,0 +1,35 @@
+# Getting Started
+
+Follow these steps to get the project up and running on your local machine.
+
+---
+
+### Prerequisites
+
+Ensure you have [Node.js](https://nodejs.org/) installed on your system.
+
+### Setup
+
+Edit public/index.html, replace TOKEN,COMPANY-ID, LOCATION-ID
+
+### Installation
+
+First, install the necessary dependencies using the Node Package Manager:
+
+```bash
+npm install
+
+```
+
+### Launch the Application
+
+Once the installation is complete, you can start the development server:
+
+```bash
+npm start
+
+```
+
+---
+
+> **Note:** The application should now be running. Check your terminal output for the local URL (usually `http://localhost:3000`).
